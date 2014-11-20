83f499505c31ad375e940c4ddf6e4281944328fa
unknown <adikha@adikha-t440p.iteroab.local>
2014-11-20 21:14:21 +0100

    ex2

---
+++ ex2.py
@@ -0,0 +1,8 @@
+#Python book reading
+#Indeed its fun to program, and o yeah we love comments (which are explainatory)
+
+print "I love programs with comments"
+
+#Yeah, for sure all good programers comment their creativity
+
+print "So, i comment my programs too"
